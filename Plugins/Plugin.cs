3c353c0da5e4100246d938cded5371fb9f273166
S4TURN <s4turn21@gmail.com>
2023-08-17 15:36:48 +0300

    feat: add Plugin abstract class

---
+++ Plugins/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace ds.test.impl
+{
+    abstract class Plugin : IPlugin
+    {
+        public string PluginName { get; private set; }
+        public string Version { get; private set; }
+        public Image Image { get; private set; }
+        public string Description { get; private set; }
+
+        public Plugin(string name, string version, Image image, string description)
+        {
+            PluginName = name;
+            Version = version;
+            Image = image;
+            Description = description;
+        }
+
+        public abstract int Run(int input1, int input2);
+    }
+}
