7f71597c1ea8f6258acfff4615d76394fa8483d9
S4TURN <s4turn21@gmail.com>
2023-08-17 15:42:04 +0300

    feat: add SubtractPlugin

---
+++ Plugins/SubtractPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl
+{
+    /// <summary>
+    /// Plugin for calculating difference between two integers
+    /// </summary>
+    class SubtractPlugin : Plugin
+    {
+        public SubtractPlugin() : base("SUB", "1.0.0", Properties.Resources.subtract_plugin_image, "Subtracts two numbers.") { }
+
+        /// <summary>
+        /// Calculates difference between two integers
+        /// </summary>
+        /// <returns>Difference between two given integers</returns>
+        public override int Run(int input1, int input2)
+        {
+            long result = input1;
+            result -= input2;
+
+            if (result > int.MaxValue || result < int.MinValue)
+            {
+                throw new OverflowException($"Result is outside the range of int values '{result}'");
+            }
+
+            return (int)result;
+        }
+    }
+}
