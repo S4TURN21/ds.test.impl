4f0df7844d38fc90caebb7c4f5486b5361dbaea6
S4TURN <s4turn21@gmail.com>
2023-08-17 15:08:23 +0300

    feat: add IPlugin interface

---
+++ IPlugin.cs
@@ -0,0 +1,26 @@
+﻿namespace ds.test.impl
+{
+    /// <summary>
+    /// Interface for plugin. Provides plugin information and allows to run operations on integers.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <value>Name of plugin</value>
+        string PluginName { get; }
+
+        /// <value>Version of plugin</value>
+        string Version { get; }
+
+        /// <value>Image of plugin</value>
+        System.Drawing.Image Image { get; }
+
+        /// <value>Description of plugin</value>
+        string Description { get; }
+
+        /// <summary>
+        /// Run operation on integers
+        /// </summary>
+        /// <returns>Operation result</returns>
+        int Run(int input1, int input2);
+    }
+}
